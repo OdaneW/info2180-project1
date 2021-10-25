448f0b304fa54ef920b64d68ed2f13c9f9d7145b
Odane Williams <odanew1@gmail.com>
2021-10-25 00:02:37 -0500

    Added index.html scripts.js styles.css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+const submiter = document.getElementsByTagName('button');
+submiter[0].addEventListener("click", function(){
+    let formItem = document.getElementById('email').value;
+    if (formItem != ''){
+        alert('Thank you! Your email address '+formItem+' has been added to our mailing list!');
+    }
+    else{
+        alert('Please enter a valid email address');
+    }
+    submiter.preventDefault();
+})
+}
